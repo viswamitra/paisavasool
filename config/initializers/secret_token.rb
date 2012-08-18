6a0a94f2118324d615bb8711e70401a7e7a1b822
Abhiram Viswamitra <abhiram.viswamitra@gmail.com>
2012-08-19 00:34:52 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paisavasool::Application.config.secret_token = 'c44eb2eda052853c2e01c187ab1e04cfe2e39a8d2e9f371f731c395e71e1b86a758c6e0fa4d5688387ba7706757f2b799815ab1199db88d5eb84539ec1a26ebd'
